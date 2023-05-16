63cb80ea1d207be54fe0b073267139723bf66875
rusheep <yo0983987618@gmail.com>
2023-05-16 12:18:33 +0800

    U6TJ61J61U4UL42;304

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start(); // 開始 Session
-    echo "登入狀態:". $_SESSION['login'] ."</br>";
-    echo "登入帳號:". $_SESSION['account'] ."</br>";
-    echo "登入權限:". $_SESSION['level'] ."</br>";
-
-?>
