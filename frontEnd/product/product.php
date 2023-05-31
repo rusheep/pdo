eb4c05f4f008182580522ddc5e4a72f7c79dcfbd
kuankuan0726 <127811787+kuankuan0726@users.noreply.github.com>
2023-05-31 13:21:56 +0800

    後台走私商人
    * 走私商人資料庫
    
    * 路徑更新

---
+++ frontEnd/product/product.php
@@ -0,0 +1,18 @@
+<?php
+include('../../Lib/conn.php');
+
+//---------------------------------------------------
+
+//建立SQL語法
+$sql = "select * from monsterdb.PRODUCT p
+join monsterdb.SIZE_TABLE s
+on p.PRODUCT_SIZE_ID = s.SIZE_ID
+order by PRODUCT_ID;";
+
+$statement = $pdo->query($sql);
+
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$json_results = json_encode($data);
+
+echo $json_results;
