4548cdc470924931b9f44727878fc4f39be6b87e
rusheep <yo0983987618@gmail.com>
2023-06-07 08:44:57 +0800

    更改顯示條件

--- frontEnd/tickOrder/tickOrderSelect.php
+++ frontEnd/tickOrder/tickOrderSelect.php
@@ -2,43 +2,37 @@
         
         include('../../Lib/conn.php');
         include('../../Lib/MemberCheck.php');
+        
         //---------------------------------------------------
         
         //取得登入的帳號
         getMemberName();
-        $account=$_SESSION["MemberAccount"];
-
-        // 用登入的帳號查詢MEMBER_ID
-        $query2 = "SELECT MEMBER_ID FROM MEMBER WHERE ACCOUNT = :account";
-        $stmt2 = $pdo->prepare($query2);
-        $stmt2->bindParam(':account', $account);
-        $stmt2->execute();
-
-        // 取出來本來是String 把結果轉成int
-        $result2 = intval($stmt2->fetch(PDO::FETCH_ASSOC)['MEMBER_ID']);
+        $account = $_SESSION["MemberAccount"];
+        $memberId = $_SESSION["MemberId"];
         
         // 查詢ORDER
-       $query1 = "SELECT * FROM `ORDER` WHERE MEMBER_ID = $result2 AND ORDER_STATE = '購物車'";
-       $result1 = $pdo->query($query1);
-       $orderIds = array();
-
-        while ($row = $result1->fetch(PDO::FETCH_ASSOC)) {
-        $orderIds[] = $row['ORDER_ID'];
-        };
-
+        $query1 = "SELECT * FROM `ORDER` WHERE MEMBER_ID = :member_id AND ORDER_STATE = '已完成'";
+        $stmt = $pdo->prepare($query1);
+        $stmt->bindParam(':member_id', $memberId);
+        $stmt->execute();
+        
+        $orderIds = array();
+        $rows = array(); // 初始化 $rows 陣列
+        
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $orderIds[] = $row['ORDER_ID'];
+        }
+        
         foreach ($orderIds as $orderId) {
-                // echo "ORDER_ID: " . $orderId . "<br>";
-                // 可以在此處執行進一步的查詢，使用 $orderId 作為條件
-
-                $orderIdsString = $orderId; // 使用當前的訂單 ID
-                $query2 = "SELECT * FROM TICK_ORDER WHERE ORDER_ID IN ($orderIdsString)";
-                $result2 = $pdo->query($query2);
-            
-                while ($row = $result2->fetch(PDO::FETCH_ASSOC)) {
-                        $rows[] = $row;
-                }
-                
-        };
+            $orderIdsString = $orderId;
+            $query2 = "SELECT * FROM TICK_ORDER WHERE ORDER_ID IN ($orderIdsString)";
+            $result2 = $pdo->query($query2);
+        
+            while ($row = $result2->fetch(PDO::FETCH_ASSOC)) {
+                $rows[] = $row;
+            }
+        }
         
         echo json_encode($rows);
+        
 ?>
