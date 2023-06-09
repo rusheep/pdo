8722e966663e232a2e3b31617ab63485e6a94b86
rusheep <yo0983987618@gmail.com>
2023-06-09 21:48:01 +0800

    更改連接資料庫路徑

--- Lib/conn.php
+++ Lib/conn.php
@@ -1,18 +1,14 @@
 <?php
-        //MySQL相關資訊
-        $db_host = "127.0.0.1";
-        $db_user = "root";
+//MySQL相關資訊
+$db_host = "127.0.0.1";
+$db_user = "tibamefe_since2021";
+$db_pass = "vwRBSb.j&K#E";
+$db_select = "tibamefe_thd101g4";
 
-        $db_pass = "yo0960797";
+//建立資料庫連線物件
+$dsn = "mysql:host=" . $db_host . ";dbname=" . $db_select . ";charset=utf8";
 
-
-        $db_select = "monsterdb";
-
-        //建立資料庫連線物件
-        $dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
-
-        //建立PDO物件，並放入指定的相關資料
-        $pdo = new PDO($dsn, $db_user, $db_pass);
+//建立PDO物件，並放入指定的相關資料
+$pdo = new PDO($dsn, $db_user, $db_pass);
 
        //---------------------------------------------------
-?>
